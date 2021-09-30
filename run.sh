3a3313491e2b5249f53cc95a49eeaa5b445e5f48
Tom PRESTON <t.preston@nodya-group.com>
2021-10-01 01:12:31 +0200

    added run.sh

---
+++ run.sh
@@ -0,0 +1,55 @@
+
+#!/bin/bash
+cecho() {
+    LB='\033[1;36m' # Light Blue
+    LG='\033[1;32m' # Light Green
+    YE='\033[1;33m' # Yellow
+
+    # print normally if no second arg added
+    if [[ $2 == "" ]]; then
+        echo $1
+        return
+    fi
+
+    case $1 in
+    BL)
+        printf "$LB%s\033[0m\n" "$2" # Light Cyan
+        ;;
+    GR)
+        printf "$LG%s\033[0m\n" "$2" # Light green
+        ;;
+    YE)
+        printf "$YE%s\033[0m\n" "$2" # Light green
+        ;;
+    normal | *)
+        echo $2 # Light Purple
+        ;;
+    esac
+}
+
+function run_up {
+    cecho "BL" "running server..."
+    docker run --rm -d -p 8080:80 --name tpprofile -v ~/projects/profile/html:/usr/share/nginx/html nginx
+}
+
+function show_help {
+    cecho "Help: $0 <ACTION>"
+    cecho "Parameters :"
+    cecho " - ACTION values :"
+    cecho "   * up                            - Run nginx."
+}
+
+if [[ "$1" == "" ]]; then
+    cecho "No arguments provided."
+    show_help
+    exit 1
+fi
+
+case "$1" in
+up)
+    run_up
+    ;;
+*)
+    show_help
+    ;;
+esac
